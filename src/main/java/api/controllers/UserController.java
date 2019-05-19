3dfe105529204ee5928ada20dce5c55d92696615
Julius Bendezu <julius.bendezu@gmail.com>
2019-05-19 15:32:50 +0200

    Stupid

--- src/main/java/api/controllers/UserController.java
+++ src/main/java/api/controllers/UserController.java
@@ -2,6 +2,7 @@ package api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class UserController {
 	}
 	
 	@GetMapping("/{username}")
-	public User getUser(String username) {
+	public User getUser(@PathVariable("username") String username) {
 		User u = userService.getUser(username);
 		if(u == null)
 			throw new UserNotFoundException();
